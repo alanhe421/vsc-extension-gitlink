6c53eabddc4f9fb277dd25a0b6b6ef547e561a90
alanhe <qianghe421@gmail.com>
2025-03-01 14:31:24 +0800

    refactor: remove unused interface definitions for Platform, DomainMapping, and DomainResult in extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -4,42 +4,9 @@ import { exec } from 'child_process';
 import * as path from 'path';
 import { promisify } from 'util';
 import * as vscode from 'vscode';
+import { Platform, DomainMapping, DomainResult } from './types';
 
 const execAsync = promisify(exec);
-
-// 定义平台和域名注册表的接口
-interface Platform {
-	/**
-	 * 平台名称，比如GitHub/腾讯Coding
-	 */
-	name: string;
-	/**
-	 * 平台URL模板，比如https://github.com/{repo:path}/blob/{branch}/{file:path}
-	 * 支持的变量：
-	 * {repo:path}：仓库路径，比如username/repo
-	 * {branch}：分支，比如main
-	 * {file:path}：文件路径，比如src/index.js
-	 * {file:name}：文件名，比如index.js
-	 * {file:dir}：文件目录，比如src
-	 * {remote:url}：远程仓库域名，比如github.com
-	 * {remote:url:path:n}：远程URL路径的第n个段（索引从0开始），比如对于git@github.com:alanhe421/alfred-workflows.git，
-	 *                      {remote:url:path:0}为alanhe421，{remote:url:path:1}为alfred-workflows
-	 * {user}：用户名或组织名（已废弃，请使用{remote:url:path:0}代替）
-	 */
-	urlTemplate: string;
-}
-
-interface DomainMapping {
-	domain: string;
-	platform: string;
-}
-
-// 从远程 URL 中提取域名的结果
-interface DomainResult {
-	domain: string;
-	remoteUrl: string;
-	pathSegments: string[]; // 路径段数组
-}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
